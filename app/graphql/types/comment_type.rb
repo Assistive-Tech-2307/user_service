6cf6f81da31eff0be5f95bf3e8bce1ebf3bff98d
Eliza Keating <ek.elizakeating@gmail.com>
2023-12-04 12:06:33 -0700

    feat: add graphql comment type

---
+++ app/graphql/types/comment_type.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  class CommentType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String
+    field :link, String
+    field :description, String
+    field :user_comment, String
+    field :rating, Boolean
+    field :user_recommended, Boolean
+    field :category_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
