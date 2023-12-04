0cb8bfcd3f092d6d7ad0e29a2f6233c12fb7ee90
Eliza Keating <ek.elizakeating@gmail.com>
2023-12-03 19:31:37 -0700

    test: model testing

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Comment, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :link }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :user_comment }
+    it { should validate_presence_of :rating }
+    it { should validate_presence_of :user_recommended }
+  end
+
+  describe "relationships" do
+    it { should belong_to :category }
+    it { should belong_to :user }
+  end
+end
