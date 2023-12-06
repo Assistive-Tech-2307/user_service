fafc197cf0d6e7c96bb5e9491b5d2c03c3d6a30c
Eliza Keating <ek.elizakeating@gmail.com>
2023-12-06 09:13:42 -0700

    feat: remove user_recommended from graphql and model

--- app/graphql/mutations/comment_create.rb
+++ app/graphql/mutations/comment_create.rb
@@ -5,20 +5,18 @@ module Mutations
     argument :description, String, required: true
     argument :user_comment, String, required: true
     argument :rating, Boolean, required: true
-    argument :user_recommended, Boolean, required: true
     argument :category, String, required: true
     argument :user_id, Integer, required: true
 
     type Types::CommentType
 
-    def resolve(title:, link:, description:, user_comment:, rating:, user_recommended:, category:, user_id:)
+    def resolve(title:, link:, description:, user_comment:, rating:, category:, user_id:)
       Comment.create!(
         title: title,
         link: link,
         description: description,
         user_comment: user_comment,
         rating: rating,
-        user_recommended: user_recommended,
         category_id: Category.find_by(title: category).id,
         user_id: user_id
       )
