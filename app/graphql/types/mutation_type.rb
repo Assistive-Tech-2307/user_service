d139c89583cbac31825a99af8a0b5a7efa620f92
Joop Stark <stark.joop@gmail.com>
2023-12-08 14:18:30 -0700

    refactor: moved session logic from mutation type to mutation folder

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,17 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     field :comment_create, mutation: Mutations::CommentCreate
     field :comment_delete, mutation: Mutations::CommentDelete
-    field :sessionCreate, Types::UserType, null: false do
-      argument :email, String, required: true
-      argument :password, String, required: true
-    end
-    def sessionCreate(email:, password:)
-      user = User.find_by(email: email)
-      if user&.authenticate(password)
-        user
-      else
-        raise GraphQL::ExecutionError.new("Invalid email or password")
-      end
-    end
+    field :sessionCreate, mutation: Mutations::SessionCreate
   end
 end
